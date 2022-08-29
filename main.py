82dbb2d90c7b8166e7504eab59c949bd10e7a277
Oscar Bucio <oscarbucio2001@gmail.com>
2022-08-29 18:46:36 -0500

    Add: regex function

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
-import math
 import re
 import keyword
 
 # Este es un compilador que pasa de un codigo en python a C++
 
 # Variables de archivos
-a = open("codigoPythonLineaPorLinea.txt", "w", encoding="utf-8") # Aquí se escribira linea por linea el archivo de entrada
+a = open("codigoPythonLineaPorLinea.txt", "w", encoding="utf-8") # Aquí se escribira linea por linea el archivo de salida
 espaciosVacios = 0
 palabrasReservadasCpp = [ "auto", "const", "double", "float", "int", "short", "struct", "unsigned", "break", "continue", "else", "for", "long", "signed", "switch", "void", "case", "default", "enum", "goto", "register", "sizeof", "typedef", "volatile", "char", "do", "extern", "if", "return", "static", "union", "while", "asm", "dynamic_cast", "namespace", "reinterpret_cast", "try", "bool", "explicit", "new", "static_cast", "typeid", "catch", "false", "operator", "template", "typename", "class", "friend", "private", "this", "using", "const_cast", "inline", "public", "throw", "virtual", "delete", "mutable", "protected", "true", "wchar_t", "and", "bitand", "compl", "not_eq", "or_eq", "xor_eq", "and_eq", "bitor", "not", "or", "xor" 
 ]
 palabrasReservadasPython = keyword.kwlist
+print(palabrasReservadasPython)
 
+# Funciones
+def revisarRe(cadena):
+    x = re.search("Aqui va la expresion regular", cadena)
+    if x:
+        print("Hay coincidencia")
+    else: 
+        print("No hay coincidencia")
+
+# Funcion principal
 def run():
     # Lectura de archivo
     f = open("ProyectoFinal.txt", "r")
@@ -21,9 +30,12 @@ def run():
     for i in f:
         if i != '\n': #Se revisa si el renglon no está vacío
             cadena = i # Equivale al renglon actual del archivo
+
             # Quitar identación
             # cadena = cadena.strip()
+
             a.write(f"{cadena}\n")
+            revisarRe(cadena)
 
             # Revisar si caracteres especiales están completos
         else:
